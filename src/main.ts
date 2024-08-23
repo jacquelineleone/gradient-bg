798b7d5ee9f1e4387a0c710b1cce227cb8e26ec3
Jacqueline <jacqueline.leone02@gmail.com>
2024-08-23 12:32:03 -0300

    add: cursor listener

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-import './style.scss'
+import "./style.scss";
+
+let curX = 0;
+let curY = 0;
+let tgX = 0;
+let tgY = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const interBubble: HTMLDivElement =
+    document.querySelector<HTMLDivElement>(".interactive")!;
+
+  function move() {
+    curX += (tgX - curX) / 20;
+    curY += (tgY - curY) / 20;
+    interBubble.style.transform = `translate(${Math.round(
+      curX
+    )}px, ${Math.round(curY)}px)`;
+    requestAnimationFrame(() => {
+      move();
+    });
+  }
+
+  window.addEventListener("mousemove", (event) => {
+    tgX = event.clientX;
+    tgY = event.clientY;
+  });
+
+  move();
+});
